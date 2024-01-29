28b3843b21146832b3c1b53e2d1be3dc1a683127
Jon <jonathan.verrier@hotmail.com>
2024-01-29 12:33:11 +0000

    Refactoring API logic ready to add fluid relay

--- api/key/index.js
+++ api/key/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
    if (req.query.JoinKey == process.env.JoinKey) {
       context.res = {
          /* Defaults to status 200 */
-         body: process.env.JoinKey
+         body: process.env.ConversationKey
       };
    }
    else {
