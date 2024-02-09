c9da17eb0ee2ee0a614d8382ce1d99d2d8a64b18
Jon <jonathan.verrier@hotmail.com>
2024-02-09 20:19:57 +0000

    Got local calls to AI working.

--- local-web.js
+++ local-web.js
@@ -3,5 +3,18 @@ var port = 1337;
 var express = require("express");
 
 var app = express();
+
+app.get('/api/joinkey', function routeHandler(req, res) {
+    if (req.query.JoinKey == process.env.JoinKey) {    
+       res.send(process.env.JoinKey);
+    }
+});
+
+app.get('/api/aikey', function routeHandler(req, res) {
+    if (req.query.JoinKey == process.env.JoinKey) {     
+       res.send(process.env.OpenAiKey);
+    }
+});
+
 app.use('/', express.static('./public'));
 app.listen(port, host);
