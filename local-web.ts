40800e5c0820cdf8b6252f6eca3b5fd71f28eba3
Jon <jonathan.verrier@hotmail.com>
2024-04-16 12:19:43 +0100

    Added new typescript API on server, just for testing typescript at present.

--- local-web.ts
+++ local-web.ts
@@ -6,33 +6,42 @@ var login = require("./api/login/index");
 var auth = require("./api/auth/index");
 var dbkey = require("./api/dbkey/index");
 
+import { messageLocalApi } from './api/message/message';
+
 var app = express();
 
-app.get('/api/joinkey', function routeHandler(req, res) {
+app.get('/api/joinkey', function routeHandler(req: any, res: any) {
     if (req.query.session == process.env.SessionKey) {    
        res.send(process.env.SessionKey);
     }
 });
 
-app.get('/api/aikey', function routeHandler(req, res) {
+app.get('/api/aikey', function routeHandler(req: any, res: any) {
     if (req.query.session == process.env.SessionKey) {           
        res.send(process.env.OpenAiKey);
     }
 });
 
-app.get('/api/dbkey', function routeHandler(req, res) {
+app.get('/api/dbkey', function routeHandler(req: any, res: any) {
 
     dbkey (null, req, res);    
 });
 
-app.get('/api/login', function routeHandler(req, res) {
+app.get('/api/login', function routeHandler(req: any, res: any) {
 
     login (null, req, res);
 });
 
-app.get('/api/auth', function routeHandler(req, res) {
+app.get('/api/auth', function routeHandler(req: any, res: any) {
 
     auth (null, req, res);
+});
+
+app.get('/api/message', async function routeHandler(req: any, res: any) {
+
+    let response = await messageLocalApi (req.query.session);
+
+    res.send (response.body);
 });
 
 app.use('/', express.static('./public'));
