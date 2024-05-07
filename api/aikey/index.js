cba41c3c8bdd909781a7c254905b8caa448a3afa
Jon <jonathan.verrier@hotmail.com>
2024-05-07 12:54:45 +0100

    Moved to Azure AI

--- api/aikey/index.js
+++ api/aikey/index.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
     if (req.query.session == process.env.SessionKey) {
        context.res = {
           /* Defaults to status 200 */
-          body: process.env.AiKey
+          body: process.env.AzureAiKey
        };
     }
     else {
