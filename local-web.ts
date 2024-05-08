3dbc46866d81ab2493db158cb5578c53d2ec709a
Jon <jonathan.verrier@hotmail.com>
2024-05-08 18:00:12 +0100

    Relevance finding now works

--- local-web.ts
+++ local-web.ts
@@ -12,24 +12,28 @@ import { messageLocalApi } from './api/message/index';
 var app = express();
 
 app.get('/api/joinkey', function routeHandler(req: any, res: any) {
+    console.log ('/api/joinkey');
     if (req.query.session == process.env.SessionKey) {    
        res.send(process.env.SessionKey);
     }
+    console.log ('/api/joinkey end');
 });
 
 app.get('/api/aikey', function routeHandler(req: any, res: any) {
+    console.log ('/api/aikey');    
     if (req.query.session == process.env.SessionKey) {           
        res.send(process.env.AzureAiKey);
     }
 });
 
 app.get('/api/mdbkey', async function routeHandler(req: any, res: any) {
-
+    console.log ('/api/mdbkey');
     await mdbkey (null, req, res);   
 });
 
 app.get('/api/cdbkey',  function routeHandler(req: any, res: any) {
 
+    console.log ('/api/cdbkey');
     if (req.query.session == process.env.SessionKey) {           
         res.send(process.env.CosmosApiKey);
      }
@@ -37,16 +41,19 @@ app.get('/api/cdbkey',  function routeHandler(req: any, res: any) {
 
 app.get('/api/login', async function routeHandler(req: any, res: any) {
 
+    console.log ('/api/login');
     await login (null, req, res);
 });
 
 app.get('/api/auth', async function routeHandler(req: any, res: any) {
 
+    console.log ('/api/auth');
     await auth (null, req, res);
 });
 
 app.get('/api/message', async function routeHandler(req: any, res: any) {
 
+    console.log ('/api/message');
     // Still cannot get this to work from azure - on hold for the moment. 
     let response = await messageLocalApi (req.query.session);
 
