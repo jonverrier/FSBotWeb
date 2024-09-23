4bd1cfde2e99c5ad6eed85cb9228ee5afef998fe
Jon <jonathan.verrier@hotmail.com>
2024-09-23 18:15:58 +0100

    Fluid connection and tests now work against API

--- local-web.js
+++ local-web.js
@@ -4,13 +4,5 @@ var express = require("express");
 
 var app = express();
 
-app.get('/api/fluidkey', async function routeHandler(req, res) {
-   
-   console.log ('/api/fluidkey');
-   if ((req.query.session === process.env.SessionKey) || (req.query.session === process.env.SessionKey2)) {
-      res.send (process.env.ConversationKey);
-   }
-});
-
 app.use('/', express.static('./public'));
 app.listen(port, host);
